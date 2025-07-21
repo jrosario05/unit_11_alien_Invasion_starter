18e33436427ba76e83c9cd25d4561e51f7c72048
throwAwayPlatanos <132602168+throwAwayPlatanos@users.noreply.github.com>
2025-07-20 22:51:21 -0400

    test commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,24 @@
 import sys
 import pygame
 
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.running = True
+
+    def run_game(self):
+        #Game loop
+        while self.running:
+            for event in pygame.event
+
+
+
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+
+
